f2995fb786d1a27ba47ecc3cf85a7b60b8fc4e26
Lonney <61323032+lonney9@users.noreply.github.com>
2021-03-24 10:54:56 -0700

    Create ipaddress-scan.sh

---
+++ ipaddress-scan.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Pings list of IP addresses from file (one per line) and logs results to a CSV file
+# Found network or hosts were blocking/evading nmap probes
+
+infile="ipaddress-list.txt"
+outfile="ipaddress-results.csv"
+
+date > "$outfile"
+echo "IP Address,State" >> "$outfile"
+
+while read output; do
+    echo -n "$output" >> "$outfile"
+    if ping -c1 "$output" > /dev/null 2>&1; then
+        echo ",UP" >> "$outfile"
+    else
+        echo ",DOWN" >> "$outfile"
+    fi
+done < $infile
